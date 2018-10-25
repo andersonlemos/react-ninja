827afd508b6688f07cd7bad92b64848c7ab1d3a2
andersonlemos <anderson.lemos@dmorj.com.br>
2018-10-25 17:21:22 -0300

    use Pure Functions and Template Strings

--- src/title.js
+++ src/title.js
@@ -3,18 +3,4 @@ import React from 'react'
 const Title = ({ name, lastname }) => (
   <h1>Olá {`${name} ${lastname}`}!</h1>
 )
-
-// const Title = React.createClass({
-//   getDefaultProps: function () {
-//     return {
-//       name: 'Desconhecido',
-//       lastname: 'Sem sobrenome'
-//     }
-//   },
-//   render: function () {
-//     return (
-//       <h1> Olá {this.props.name + ' ' + this.props.lastname}! </h1>
-//     )
-//   }
-// })
 export default Title
