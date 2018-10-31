b686d245e21b282a72b229e7aea281180fd7a329
andersonlemos <anderson.lemos@dmorj.com.br>
2018-10-31 17:08:36 -0300

    second parameter index was changed in key prop

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ class App extends Component {
   render () {
     return (
       <div className='container' >
-        {['red', 'orange', 'yellow', 'green', 'blue', 'purple'].map((square) => (
-          <Square key={square} color={square} />
+        {['red', 'orange', 'yellow', 'green', 'blue', 'purple'].map((square, index) => (
+          <Square key={index} color={square} />
         ))}
       </div>
     )
