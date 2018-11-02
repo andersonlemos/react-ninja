808b81b01b7fbb9bf2cf5ddada2987d7850dfa62
Anderson Lemos <bdl182@openmailbox.org>
2018-11-02 00:27:14 -0300

    how to use a basic composition to create controls

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 'use strict'
 import React, { Component } from 'react'
-import Button from './button'
+import LikeButton from './likeButton'
+import SearchButton from './searchButton'
 class App extends Component {
   render () {
     return (
       <div className='container' >
-        <Button>
-          <span>Texto</span>
-        </Button>
+        <LikeButton />
+        <SearchButton />
       </div>
     )
   }
